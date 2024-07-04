a55add4b6d769423dc4a0dd37680c2fb08186e82
tienkk <tiendong2kk2@gmail.com>
2024-07-04 15:28:04 +0700

    done

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val link = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/"))
+        val activity2 =Intent(this, Activity2::class.java)
+        binding.btnGo1.setOnClickListener {
+            startActivity(activity2)
+        }
+
+        binding.btngo2.setOnClickListener {
+            startActivity(link)
+        }
+    }
+}
